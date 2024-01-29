e173c02ae5771a3b810faf87e4967d88184361dd
lohanvictor <lohan.victor@hotmail.com>
2024-01-29 16:20:20 -0400

    refactoring chat/contact-list styles

--- src/ContactList.js
+++ src/ContactList.js
@@ -4,11 +4,13 @@ import { io } from 'socket.io-client'
 import camellia from './camellia.js'
 import RSAHandler from './rsaKeyGeneration.js'
 import { Typography } from '@mui/material'
-import { useUser } from './hook/useUser.js';
-import {useCurrentUser} from './hook/usersContactList.js';
-import { useChat } from './hook/useChat.js';
-import {useSession} from './hook/useSession.js';
-import ContactsIcon from '@mui/icons-material/Contacts';
+import { useUser } from './hook/useUser.js'
+import { useCurrentUser } from './hook/usersContactList.js'
+import { useChat } from './hook/useChat.js'
+import { useSession } from './hook/useSession.js'
+import ContactsIcon from '@mui/icons-material/Contacts'
+
+import { useUserChat } from './contexts/useUserChat.js'
 
 const SERVER_URL = 'http://localhost:8000'
 export default function ContactList() {
@@ -17,15 +19,18 @@ export default function ContactList() {
     const [users, setUsers] = useState([])
     const [selectedUserId, setSelectedUserId] = useState(null) // State to track selected user ID
     const [currentChatUser, setCurrentChatUser] = useState(null)
-    const {CurrentUser, setCurrentUser}= useCurrentUser()
+    const { CurrentUser, setCurrentUser } = useCurrentUser()
 
     const socketRef = useRef()
-    const { CurrentUserPUblicKey, setCurrentUserPublicKey} = useUser()
+    const { CurrentUserPUblicKey, setCurrentUserPublicKey } = useUser()
     // const {currentSession, setCurrentSession} = useSession()
-    const {currentSession, setCurrentSession, userSessions, setUserSessions} = useSession()
+    const { currentSession, setCurrentSession, userSessions, setUserSessions } =
+        useSession()
 
-    const { messages, setMessages} = useChat()
-    
+    const { messages, setMessages } = useChat()
+
+    const { changeUsername } = useUserChat()
+
     useEffect(() => {
         socketRef.current = io(SERVER_URL, {
             transports: ['websocket'],
@@ -33,13 +38,13 @@ export default function ContactList() {
         })
 
         socketRef.current.on('connect', () => {
-          console.log('Connect');
-        });
+            console.log('Connect')
+        })
 
         socketRef.current.on('usersList', async (usersList) => {
-          console.log('USER LIST', usersList);
-            const storedUsername = sessionStorage.getItem('username');
-            console.log('STORED USER', storedUsername);
+            console.log('USER LIST', usersList)
+            const storedUsername = sessionStorage.getItem('username')
+            console.log('STORED USER', storedUsername)
             const otherUsers = usersList.filter(
                 (user) => user.username !== storedUsername
             )
@@ -61,28 +66,27 @@ export default function ContactList() {
     }
     function base64ToArrayBuffer(base64) {
         try {
-            console.log("raw base64")
+            console.log('raw base64')
             console.log(base64)
-            const cleanedBase64 = base64.trim().replace(/\s/g, '');
-            console.log("Cleaned base64")
+            const cleanedBase64 = base64.trim().replace(/\s/g, '')
+            console.log('Cleaned base64')
             console.log(cleanedBase64)
-            const binaryString = window.atob(cleanedBase64);
+            const binaryString = window.atob(cleanedBase64)
             if (binaryString != null) {
-              const len = binaryString.length;
-              const bytes = new Uint8Array(len);
-              for (let i = 0; i < len; i++) {
-                bytes[i] = binaryString.charCodeAt(i);
+                const len = binaryString.length
+                const bytes = new Uint8Array(len)
+                for (let i = 0; i < len; i++) {
+                    bytes[i] = binaryString.charCodeAt(i)
+                }
+                return bytes.buffer
+
+                // ... rest of your code ...
+            } else {
+                console.error('binaryString is undefined')
+                console.log(binaryString)
             }
-            return bytes.buffer;
-    
-              // ... rest of your code ...
-          } else {
-              console.error('binaryString is undefined');
-              console.log(binaryString)
-          }
-            
         } catch (error) {
-            console.error('Error decoding Base64 string:', error);
+            console.error('Error decoding Base64 string:', error)
             // Handle error or return a fallback
         }
     }
@@ -99,9 +103,12 @@ export default function ContactList() {
             false,
             ['decrypt']
         )
-        try{
-            const decryptedKey = await RSAHandler.rsaDecrypt(importedPrivateKey, keyBuffer);
-            
+        try {
+            const decryptedKey = await RSAHandler.rsaDecrypt(
+                importedPrivateKey,
+                keyBuffer
+            )
+
             const plainTextBuffer = camellia.decrypt(
                 text,
                 decryptedKey,
@@ -111,25 +118,21 @@ export default function ContactList() {
             console.log('Camellia been executed successfully')
             console.log('Decrypted Message:', plainTextBuffer)
             return plainTextBuffer
-
-    
-        }
-        catch (error) {
-            console.error('Error decrypting the key:', error);
+        } catch (error) {
+            console.error('Error decrypting the key:', error)
             // return "Error: " + error
             // Handle error or return a fallback
         }
         // Assuming 'text' is the encrypted message
         // const textBuffer = base64ToArrayBuffer(text)
-        
     }
 
     const handleUserClick = async (user) => {
         setCurrentUser(user.id_)
-        console.log('User clicked');
+        console.log('User clicked')
         console.log(doesSessionExist(user.id_))
         if (!doesSessionExist(user.id_)) {
-            console.error('Session does not exist');
+            console.error('Session does not exist')
             const sessionId = generateSessionId() // Your session ID generation logic
             userSessions[user.id_] = sessionId
             // setUserSessions((prevSessions) => ({
@@ -159,62 +162,57 @@ export default function ContactList() {
             )
             if (response.ok) {
                 const data = await response.json()
-                
+
                 if (data.length > 0) {
-                const decryptedMessages = await Promise.all(
-                    data.map(async (message) => {
-                        console.log("mensagem do historico:")
-                        console.log(message.text)
-                        console.log("Chave simétrica criptografada")
-                        console.log(message.key)
-                        const {text, key, sender} = message
-                        if (sender == user.id_){
-                            const decryptedText = await decryptMessages(
-                                text,
-                                key
-                            )
-                            console.log('decryption function was executed')
-                            console.log(decryptedText)
-                            const depryptedMessage = {
-                                text: decryptedText,
-                                sender: sender
+                    const decryptedMessages = await Promise.all(
+                        data.map(async (message) => {
+                            console.log('mensagem do historico:')
+                            console.log(message.text)
+                            console.log('Chave simétrica criptografada')
+                            console.log(message.key)
+                            const { text, key, sender } = message
+                            if (sender == user.id_) {
+                                const decryptedText = await decryptMessages(
+                                    text,
+                                    key
+                                )
+                                console.log('decryption function was executed')
+                                console.log(decryptedText)
+                                const depryptedMessage = {
+                                    text: decryptedText,
+                                    sender: sender,
+                                }
+                                return depryptedMessage
+                            } else {
+                                const { text, key, sender, sender_key } =
+                                    message
+                                const decryptedText = await decryptMessages(
+                                    text,
+                                    sender_key
+                                )
+                                console.log('decryption function was executed')
+                                console.log(decryptedText)
+                                const depryptedMessage = {
+                                    text: decryptedText,
+                                    sender: sender,
+                                }
+                                return depryptedMessage
                             }
-                            return depryptedMessage
+                        })
+                    )
+                    console.log('Messages from history')
+                    console.log(decryptedMessages)
+                    setMessages(decryptedMessages)
 
-                        }
-                        else{
-                            const {text, key, sender, sender_key} = message
-                            const decryptedText = await decryptMessages(
-                                text,
-                                sender_key
-                            )
-                            console.log('decryption function was executed')
-                            console.log(decryptedText)
-                            const depryptedMessage = {
-                                text: decryptedText,
-                                sender: sender
-                            }
-                            return depryptedMessage
-                        }
-                    
-                     
-                    })
-                )
-                console.log("Messages from history")
-                console.log(decryptedMessages)
-                setMessages(decryptedMessages);
-
-                // setMessages(decryptedMessages)
-                }
-                else{
-                    setMessages([]);
+                    // setMessages(decryptedMessages)
+                } else {
+                    setMessages([])
                 }
                 // You can now use this publicKey for further operations
             } else {
                 console.error('History:', response.status)
             }
         } catch (error) {
-           
             console.error('Error fetching history:', error)
         }
 
@@ -228,8 +226,9 @@ export default function ContactList() {
                 setCurrentUser(data.id_)
                 setCurrentUserPublicKey(publicKey)
                 console.log('Public key changed')
-       
+
                 console.log('Public Key for', user, ':', user.public_key)
+                changeUsername(user.username)
             } else {
                 console.error('Failed to fetch public key:', response.status)
             }
@@ -247,19 +246,32 @@ export default function ContactList() {
                 bgcolor="#9c6fe487"
                 overflowY="auto"
                 minWidth="240px"
-                borderRadius= "0px 0px 0px 8px"
+                borderRadius="0px 0px 0px 0px"
             >
-                
-                <Typography
-                    
-                    variant="h4"
-                    sx={{ color: "white", padding: '10px', fontFamily:'Poppins', alignItems:'center'}}
-                    
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        height: '60px',
+                        gap: '8px',
+                        borderBottom: '1px solid #fff'
+                    }}
                 >
-                    <ContactsIcon sx={{ fontSize: 40, color: 'white' }} />  {/* Adiciona o ícone */}
-                       Contacts Online
-                </Typography>
-                <ul style={{ listStyle: 'none', padding: 0 }}>
+                    <ContactsIcon sx={{ fontSize: 38, color: 'white' }} />
+                    <Typography
+                        variant="h5"
+                        sx={{
+                            color: 'white',
+                            fontFamily: 'Poppins',
+                        }}
+                        lineHeight="10px"
+                        fontSize="24px"
+                    >
+                        Contacts Online
+                    </Typography>
+                </div>
+                <ul style={{ listStyle: 'none', padding: 0, margin: 0 }}>
                     {users.map((user, index) => (
                         <li
                             key={index}
@@ -273,12 +285,19 @@ export default function ContactList() {
                                 cursor: 'pointer',
                                 borderTop: 'none',
                                 borderRight: 'none',
-                                borderColor: "#9c6fe487",
-                                fontFamily:'Poppins', 
+                                borderColor: '#9c6fe487',
+                                fontFamily: 'Poppins',
                             }}
                             onClick={() => handleUserClick(user)}
                         >
-                            {user.username}
+                            <span
+                                style={{
+                                    color: 'white',
+                                    fontSize: '16px',
+                                }}
+                            >
+                                {user.username}
+                            </span>
                         </li>
                     ))}
                 </ul>
--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,23 @@
-import { Box } from "@mui/material";
-import React from "react";
-import ContactList from "./ContactList";
-import Chat from "./Chat";
+import { Box } from '@mui/material'
+import React from 'react'
+import ContactList from './ContactList'
+import Chat from './Chat'
+import { UserChatProvider } from './contexts/useUserChat'
 
 const Home = () => {
-  return (
-    <Box display="flex"width="100%" flexDirection="row" height="100vh">
-      <ContactList />
-      <Box
-        sx={{
-          border: ".05px solid #9c6fe433",
-        }}
-      />
-      <Chat />
-    </Box>
-  );
-};
+    return (
+        <UserChatProvider>
+            <Box display="flex" width="100%" flexDirection="row" height="100vh">
+                <ContactList />
+                <Box
+                    sx={{
+                        border: '.05px solid #9c6fe433',
+                    }}
+                />
+                <Chat />
+            </Box>
+        </UserChatProvider>
+    )
+}
 
-export default Home;
+export default Home
