6f21fc45020b5489c1e041b26226983f26b3fba0
gabisteph <gabriellepiresmestrinho@gmail.com>
2024-02-20 15:39:13 -0400

    alteracoes no index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const root = ReactDOM.render((
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
