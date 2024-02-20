f7a56ccfe968919257d669f80ade4d2a1859abb4
gabisteph <gabriellepiresmestrinho@gmail.com>
2024-02-20 15:42:05 -0400

    alteracoes no index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.render((
   <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
-);
+),)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
