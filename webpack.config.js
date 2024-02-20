86ae746fbee814eda714b1164ecb79843dc2ea28
nadila_azevedo <ndsda.eng19@uea.edu.br>
2024-02-20 00:42:33 -0300

    feat: add webpack file

---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+devServer: {
+    allowedHosts: 'https://tranquil-escarpment-24882-caea67df61ed.herokuapp.com/',
+    // Or specify hostnames:
+    // allowedHosts: ['your-app.herokuapp.com', 'localhost', '127.0.0.1']
+}  
